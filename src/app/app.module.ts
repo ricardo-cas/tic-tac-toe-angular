1c2e636dee2ca1e53669d1ac7400f4e8c3527f37
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-03-29 18:52:57 -0300

    adde styling usiing angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { GameComponent } from './game/game.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { GameComponent } from "./game/game.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+import { MatButtonModule } from "@angular/material/button";
 @NgModule({
-  declarations: [
-    AppComponent,
-    GameComponent
-  ],
+  declarations: [AppComponent, GameComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  // utilizando um seletor, pegando o elemento html
+  selector: "app-game",
+  // podemo usar também um seletor de css/scss
+  // selector: '.app-game',
+  templateUrl: "./game.component.html",
+  styleUrls: ["./game.component.scss"],
 })
 export class GameComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
